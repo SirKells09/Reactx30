802a9e855998c60afbdbf55d52c04ff2b781aab3
Kelly Williams <Kewilliams591@gmail.com>
2018-12-14 20:30:45 -0500

    changes

--- src/Components/Week1/ProgressLog/ProgressLog.js
+++ src/Components/Week1/ProgressLog/ProgressLog.js
@@ -6,21 +6,16 @@ class ProgressLog extends Component {
     render() {
         return (
             <div className="notificationsFrame">
+
+                <textarea
+                />
                 <div className="panel">
                     <div className="header">
 
-                        <div className="menuIcon">
-                            <div className="dashTop"></div>
-                            <div className="dashBottom"></div>
-                            <div className="circle"></div>
-                        </div>
 
-                        <span className="title">Timeline</span>
 
-                        <input
-                            type="text"
-                            className="searchInput"
-                            placeholder="Search ..." />
+                        <span className="title">ProgressLog</span>
+
 
                         <div className="fa fa-search searchIcon"></div>
                     </div>
@@ -28,46 +23,33 @@ class ProgressLog extends Component {
                         <div className="line"></div>
                         <div className="item">
 
-                            <div className="avatar">
-                                <img
-                                    alt='doug'
-                                    src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
-                            </div>
+
 
                             <span className="time">
                                 An hour ago
               </span>
-                            <p>Ate lunch</p>
+                            <p></p>
                         </div>
 
                         <div className="item">
-                            <div className="avatar">
-                                <img
-                                    alt='doug' src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
-                            </div>
+
 
                             <span className="time">10 am</span>
-                            <p>Read Day two article</p>
+                            <p></p>
                         </div>
 
                         <div className="item">
-                            <div className="avatar">
-                                <img
-                                    alt='doug' src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
-                            </div>
+
 
                             <span className="time">10 am</span>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <p></p>
                         </div>
 
                         <div className="item">
-                            <div className="avatar">
-                                <img
-                                    alt='doug' src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
-                            </div>
+
 
                             <span className="time">2:21 pm</span>
-                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <p></p>
                         </div>
 
                     </div>
--- src/Components/siteStructure/Home.js
+++ src/Components/siteStructure/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import ProgressLog from '../Week1/ProgressLog/ProgressLog'
 
+let spacing = {
+    height: '2em'
+}
 
 export default class Home extends Component {
     render() {
@@ -8,7 +11,10 @@ export default class Home extends Component {
             <div className="main">
                 <div className="mainDiv">
                     <h1>Welcome to 30 React Apps</h1>
+                    <div style={spacing}></div>
+
                     <ProgressLog />
+
 
 
                 </div>
